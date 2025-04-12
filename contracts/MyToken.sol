5f65d5da01f8e4ddd56df4e3fae1d641b660ea94
github hwkkkk <gudnr0504@naver.com>
2025-04-13 01:33:57 +0900

    added_transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -10,29 +10,23 @@ contract MyToken {
     mapping (address => uint256) public balanceOf;
 
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        _mint(1*10**uint256(decimals), msg.sender);
+        _mint(_amount *10**uint256(decimals), msg.sender);
     }
 
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] = balanceOf[owner] + amount;
     }
-    //transaction
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+    }
     
-    // function totalSupply() external view returns (uint256 totalSupply) {
-    //     return totalSupply;
-    // }
-
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf[owner];
-    // }
-
-    // function name() external view returns (string memory) {
-    //     return name;
-    // }
 }
 
